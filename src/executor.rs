8321d2bd934e81daef5af82729a0baaf1f083cf8
GNUSheep <pentet221@gmail.com>
2024-02-12 17:20:29 +0100

    Implement cd: built-in function

--- src/executor.rs
+++ src/executor.rs
@@ -1,8 +1,32 @@
 use std::process::Command;
+use std::path::Path;
+use std::env;
 
 use crate::parser;
 
 pub fn exec_command(cmd: parser::Command) {
+    match cmd.name.as_str() {
+        "cd" => {
+            let mut path = Path::new("/");
+            match cmd.args.len() {
+                0 => (),
+                1 => {
+                    path = Path::new(&cmd.args[0]);
+                }
+                _ => {
+                    println!("To many directions provided; see cd man");
+                    return
+                }
+            };
+            if let Err(_) = env::set_current_dir(&path) {
+                println!("Problem with changing directory");
+            }
+
+            return 
+        }
+        _ => (),
+    }
+
     let child = Command::new(cmd.name).args(cmd.args).spawn();
 
     match child {
