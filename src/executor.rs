e7020fde809de6ae5e62ad3a90c780c3c66ad2ec
GNUSheep <pentet221@gmail.com>
2024-02-15 20:28:57 +0100

    Implement pipes, single one: |

--- src/executor.rs
+++ src/executor.rs
@@ -1,4 +1,5 @@
-use std::process::{Command, exit};
+use std::process::{Command, exit, Stdio, Child};
+use std::collections::VecDeque;
 use std::path::Path;
 use std::env;
 
@@ -11,46 +12,62 @@ fn get_env(name: String) -> String {
     }
 }
 
-pub fn exec_command(mut cmd: parser::Command) {
-    match cmd.name.as_str() {
-        "cd" => {
-            let mut path = get_env("HOME".to_string());
-            if path == "" {
-                path = "/".to_string();
-            }
+pub fn exec_command(mut cmds: VecDeque<parser::Command>) {
+    let mut prev_cmd = None;
 
-            match cmd.args.len() {
-                0 => (),
-                1 => {
-                    path = cmd.args.pop().unwrap();
+    while let Some(mut cmd) = cmds.pop_front() {
+        match cmd.name.as_str() {
+            "cd" => {
+                let mut path = get_env("HOME".to_string());
+                if path == "" {
+                    path = "/".to_string();
                 }
-                _ => {
-                    println!("To many directions provided; see cd man");
-                    return
+    
+                match cmd.args.len() {
+                    0 => (),
+                    1 => {
+                        path = cmd.args.pop().unwrap();
+                    }
+                    _ => {
+                        println!("To many directions provided; see cd man");
+                        return
+                    }
+                };
+    
+                let path = Path::new(&path);
+    
+                if let Err(_) = env::set_current_dir(&path) {
+                    println!("Problem with changing directory");
                 }
-            };
-
-            let path = Path::new(&path);
-
-            if let Err(_) = env::set_current_dir(&path) {
-                println!("Problem with changing directory");
+    
+                return 
             }
-
-            return 
+            "" => return,
+            "exit" => exit(0),
+            _ => (),
         }
-        "" => return,
-        "exit" => exit(0),
-        _ => (),
+    
+        let mut stdin = prev_cmd.map_or(Stdio::inherit(), |out: Child| Stdio::from(out.stdout.unwrap()));
+
+        let mut stdout = Stdio::inherit();
+        if !cmds.is_empty() {
+            stdout = Stdio::piped();
+        }
+
+        let child = Command::new(cmd.name).args(cmd.args).stdout(stdout).stdin(stdin).spawn();
+
+        match child {
+            Ok(mut child) => {
+                child
+                    .wait()
+                    .expect("Problem with waiting for child");
+                prev_cmd = Some(child);
+                
+            }
+            Err(err) => {
+                println!("Problem with executing command: {}", err);
+                prev_cmd = None;
+            }
+        };
     }
-
-    let child = Command::new(cmd.name).args(cmd.args).spawn();
-
-    match child {
-        Ok(child) => {
-            let _ = child
-                .wait_with_output()
-                .expect("Problem with waiting for child");
-        }
-        Err(err) => println!("Problem with executing command: {}", err),
-    };
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,5 +1,6 @@
 use std;
 use std::process::exit;
+use std::collections::VecDeque;
 
 pub struct Command {
     pub name: String,
@@ -15,7 +16,7 @@ impl Command {
     }
 }
 
-pub fn parse_input() -> Command {
+pub fn parse_input() -> VecDeque<Command> {
     let mut user_input = String::new();
 
     let _ = match std::io::stdin().read_line(&mut user_input) {
@@ -27,15 +28,23 @@ pub fn parse_input() -> Command {
         Err(error) => panic!("Problem parsing user input, {:?}", error),
     };
 
-    let mut command: Command = Command::new();
-    if let Some((name, args)) = user_input
-        .split_whitespace()
-        .collect::<Vec<_>>()
-        .split_first()
-    {
-        command.name = name.to_string();
-        command.args = args.iter().map(|v| v.to_string()).collect();
+    let cmd_vec: Vec<_> = user_input.trim().split("|").collect();
+    
+    let mut commands: VecDeque<Command> = Default::default();
+    for cmd in cmd_vec {
+        let mut command: Command = Command::new();
+
+        if let Some((name, args)) = cmd
+            .split_whitespace()
+            .collect::<Vec<_>>()
+            .split_first()
+        {
+            command.name = name.to_string();
+            command.args = args.iter().map(|v| v.to_string()).collect();
+        }
+
+        commands.push_back(command);
     }
 
-    command
+    commands
 }
